a4b4641ec92ef940642197b02526be7d0adbb388
RodriiAndino <andinorodrigo03@gmail.com>
2023-12-13 12:59:21 -0300

    add: header, colors variable and config for language

---
+++ src/i18n/ui.js
@@ -0,0 +1,22 @@
+export const languages = {
+  es: 'Español',
+  en: 'English'
+}
+
+export const defaultLanguage = 'es'
+
+export const ui = {
+  es: {
+    'nav.home': 'Sobre mi',
+    'nav.skills': 'Experiencia',
+    'nav.projects': 'Proyectos',
+    'nav.contact': 'Contacto'
+  },
+
+  en: {
+    'nav.home': 'About me',
+    'nav.skills': 'Experience',
+    'nav.projects': 'Projects',
+    'nav.contact': 'Contact'
+  }
+}
