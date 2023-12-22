d85fe79eb32da9817fc192b5a9b12adac4bc6d45
RodriiAndino <andinorodrigo03@gmail.com>
2023-12-22 16:18:29 -0300

    Add: Footer, Experience, Projects, Skills, Contact, and Nav components format basic

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -10,13 +10,21 @@ export const ui = {
     'nav.home': 'Sobre mi',
     'nav.skills': 'Experiencia',
     'nav.projects': 'Proyectos',
-    'nav.contact': 'Contacto'
+    'nav.contact': 'Contacto',
+    'title.experience': 'Experiencia',
+    'title.projects': 'Proyectos',
+    'title.skills': 'Mis Habilidades',
+    'title.contact': 'Contactame'
   },
 
   en: {
     'nav.home': 'About me',
     'nav.skills': 'Experience',
     'nav.projects': 'Projects',
-    'nav.contact': 'Contact'
+    'nav.contact': 'Contact',
+    'title.experience': 'Experience',
+    'title.projects': 'Projects',
+    'title.skills': 'My Skills',
+    'title.contact': 'Contact me'
   }
 }
