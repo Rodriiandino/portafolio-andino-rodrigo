0ccf104bb92c963d92955f22845b59ab9e8f4d2a
RodriiAndino <andinorodrigo03@gmail.com>
2024-02-01 15:37:15 -0300

    Add Vercel configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
-
+import vercel from '@astrojs/vercel/serverless'
 import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
+  adapter: vercel(),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
